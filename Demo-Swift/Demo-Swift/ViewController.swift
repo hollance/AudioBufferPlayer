c21bfb31432d1dc6b9f5fb972cc42e3e27e15358
Kelvin Lau <kelvin.kl.lau@gmail.com>
2018-01-11 20:22:38 -0800

    Initial commit for Swift project.

---
+++ Demo-Swift/Demo-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo-Swift
+//
+//  Created by Kelvin Lau on 2018-01-11.
+//  Copyright © 2018 Kelvin Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
